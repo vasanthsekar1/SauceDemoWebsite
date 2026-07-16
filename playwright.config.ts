5f7b891c4e72923db7a15cd2eb14029b5c54a5a2
Vasanthakumar S <Vasanthakumar.S@coforge.com>
2026-07-16 10:55:37 +0530

    Updated .env

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
     }]],
   use: {
     baseURL: process.env.BASE_URL,
-    headless: false,
+    headless: process.env.HEADLESS === 'true',
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
     trace: 'on-first-retry',
